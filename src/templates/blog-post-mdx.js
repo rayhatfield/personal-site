7539b885276500d6fe60c4fe705d409702c414c1
Ray Hatfield <ray.hatfield@gmail.com>
2020-01-17 22:57:31 -0600

    set window title from post

--- src/templates/blog-post-mdx.js
+++ src/templates/blog-post-mdx.js
@@ -1,12 +1,16 @@
 import React from "react"
 import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
+import {Helmet} from 'react-helmet';
 
 import styles from './blog-post.module.css';
 
 export default function PageTemplate({data: {mdx}}) {
 	return (
 		<div className={styles.root}>
+			<Helmet>
+				<title>{mdx.frontmatter.title}</title>
+			</Helmet>
 			<h1>{mdx.frontmatter.title}</h1>
 			<MDXRenderer>{mdx.body}</MDXRenderer>
 		</div>
