d565037a2d86e257b46d3cfe17eb97af2a9396e3
Ray Hatfield <ray.hatfield@gmail.com>
2020-02-05 12:12:21 -0600

    don't assume frontmatter is present

--- src/templates/blog-post-mdx/components/header.jsx
+++ src/templates/blog-post-mdx/components/header.jsx
@@ -8,7 +8,8 @@ import Byline from './byline';
 import styles from './header.module.css';
 
 export default function Header ({frontmatter, classes}) {
-	let featuredImgFluid = frontmatter?.featuredImage?.childImageSharp?.fluid;
+	const featuredImgFluid = frontmatter?.featuredImage?.childImageSharp.fluid;
+	const title = frontmatter?.title;
 
 	console.log(rhythm(1), scale());
 
@@ -18,7 +19,7 @@ export default function Header ({frontmatter, classes}) {
 				<Img className={styles.featuredImage} fluid={featuredImgFluid} style={{maxHeight: rhythm(4), marginBottom: rhythm(1)}} />
 			)}
 			<h1>{frontmatter.title}</h1>
-			<Byline {...frontmatter} />
+			<Byline {...(frontmatter || {})} />
 		</header>
 	);
 }
