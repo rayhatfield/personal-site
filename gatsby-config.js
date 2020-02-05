805e82d18ae0ed41a2cf001a32f8cbb28618ed5c
Ray Hatfield <ray.hatfield@gmail.com>
2020-02-05 12:14:57 -0600

    cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,8 +14,8 @@ module.exports = {
     }
   },
   plugins: [
-    'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
