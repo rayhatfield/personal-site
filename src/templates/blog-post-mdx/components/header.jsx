c42fd31e7f53427ff2e0a26c405f989f4e04c121
Ray Hatfield <ray.hatfield@gmail.com>
2020-02-17 17:50:06 -0600

    guard against null on featured image

--- src/templates/blog-post-mdx/components/header.jsx
+++ src/templates/blog-post-mdx/components/header.jsx
@@ -8,13 +8,15 @@ import Byline from './byline';
 import styles from './header.module.css';
 
 export default function Header ({frontmatter, classes}) {
-	let featuredImgFluid = frontmatter.featuredImage.childImageSharp.fluid;
+	let featuredImgFluid = frontmatter?.featuredImage?.childImageSharp?.fluid;
 
 	console.log(rhythm(1), scale());
 
 	return (
 		<header className={styles.postHeader}>
-			<Img className={styles.featuredImage} fluid={featuredImgFluid} style={{maxHeight: rhythm(4), marginBottom: rhythm(1)}} />
+			{featuredImgFluid && (
+				<Img className={styles.featuredImage} fluid={featuredImgFluid} style={{maxHeight: rhythm(4), marginBottom: rhythm(1)}} />
+			)}
 			<h1>{frontmatter.title}</h1>
 			<Byline {...frontmatter} />
 		</header>
