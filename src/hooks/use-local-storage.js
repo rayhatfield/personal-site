4f8a5d7e427d7074cff84795b33a58c80490fe5e
Ray Hatfield <ray.hatfield@gmail.com>
2020-02-17 18:02:02 -0600

    ensure localStorage is available

--- src/hooks/use-local-storage.js
+++ src/hooks/use-local-storage.js
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 
 export default function useStorage (key, initial) {
-	const [state, setState] = useState(global.localStorage.getItem(key) || initial);
+	const [state, setState] = useState(global.localStorage?.getItem(key) || initial);
 	return [
 		state,
 		value => {
