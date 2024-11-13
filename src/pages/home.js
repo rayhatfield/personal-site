ad27169ef047d37d55ad1ddd805e1954704c65ce
ray hatfield <ray.hatfield@gmail.com>
2024-11-13 12:29:32 -0600

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,7 @@ import styles from './home.module.css';
 export default () => (
 	<main className={styles.root}>
 		<p>nothing to see here.</p>
-		<p>sometimes i <a href="https://twitter.com/rayhatfield">tweet</a>.</p>
+		<p>i'm <a href="https://bsky.app/profile/ray.wtf">@ray.wtf</a> on bluesky.</p>
 		<p>i’m <a rel="me" href="https://mastodon.online/@rayhatfield">on mastodon</a> too.</p>
 	</main>
 );
